6256350724ed571b6b90a8c3760627e00c98a665
sgraepler <s.graepler@studserv.uni-leipzig.de>
2025-03-26 18:13:20 +0100

    new version with ctk player for presi_einwecken. TODO: update the others too

--- presi_einwecken/videopresi.py
+++ presi_einwecken/videopresi.py
@@ -1,43 +1,26 @@
 from flask import Flask, render_template, jsonify, request
-import vlc
-import time
+from video_player import VideoPlayer
+from multiprocessing import Process
 
 app = Flask(__name__)
 
-# Initialize VLC media player instance
-vlc_instance = vlc.Instance("--no-xlib")
-media_player = vlc_instance.media_player_new()
 
 # Define a route for the HTML page
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 # Define a route to start playing the video
 @app.route("/play_video")
 def play_video():
-    #der Dateipfad zum Video wird in "ID" übergeben
-    videoURL = request.args.get("ID")
-    # Load the video file into the media player
-    media = vlc_instance.media_new(videoURL)
-    media_player.set_media(media)
+    video_path = request.args.get("ID")
 
-    # Set up fullscreen mode
-    media_player.set_fullscreen(True)
+    process = Process(target=VideoPlayer.start, args=(video_path,))
+    process.start()
 
-    # Play the video
-    media_player.play()
-
-    while True:
-        media_player.set_fullscreen(True)
-        if media_player.get_state() == vlc.State.Ended or media_player.get_state() == vlc.State.Stopped:
-            break
-        time.sleep(0.1)
-    
-    media_player.stop()
-
-    return jsonify(success=True)
+    return jsonify(message="Video is playing")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
